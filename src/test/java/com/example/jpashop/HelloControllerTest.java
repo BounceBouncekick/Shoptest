b07ba4a9dc4e3089ccc6464d527580a05760b3b5
unknown <agumon43@naver.com>
2024-07-12 11:34:20 +0900

    테스트 DB(h2) 연결 및  기본 테스트 구현

---
+++ src/test/java/com/example/jpashop/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.jpashop;
+
+import com.example.jpashop.controller.HelloController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloTest() throws Exception{
+        String hello = "hello";
+
+        mvc.perform((MockMvcRequestBuilders.get("/hello")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+
+}
