a84411862b36341215682c6fda55cb10efb43998
unknown <agumon43@naver.com>
2024-07-25 18:15:31 +0900

    [Fix] - hello Controller 테스트 코드 수정 및 코드리뷰

--- src/main/java/com/example/jpashop/service/ProductService.java
+++ src/main/java/com/example/jpashop/service/ProductService.java
@@ -7,6 +7,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,15 +23,28 @@ public class ProductService {
 
         private final ProductRepository productRepository;
 
-        public void create(ProductDto productDTO) throws IOException {
-            Product product = Product.toDTO(productDTO);
+        private final PlatformTransactionManager transactionManager;
 
-            log.info("product id = {}", product.getId());
-            log.info("product price = {}", product.getPrice());
-            log.info("product stock = {}", product.getStockQuantity());
-            log.info("product boardWriter = {}", product.getBoardwriter());
-            log.info("product productname = {}", product.getProductname());
-            productRepository.save(product);
+        public void create(ProductDto productDTO) throws IOException {
+
+
+            TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+            try {
+                Product product = Product.toDTO(productDTO);
+
+                log.info("product id = {}", product.getId());
+                log.info("product price = {}", product.getPrice());
+                log.info("product stock = {}", product.getStockQuantity());
+                log.info("product boardWriter = {}", product.getBoardwriter());
+                log.info("product productname = {}", product.getProductname());
+                productRepository.save(product);
+
+                transactionManager.commit(status); // 성공 시 커밋
+                } catch (Exception e) {
+                 transactionManager.rollback(status); // 실패 시 롤백
+                throw new IllegalStateException(e);
+            }
         }
 
         public List<Product> findItems(){
@@ -39,24 +55,43 @@ public class ProductService {
             return productRepository.findByUuid(uuid);
         }
 
-        public void update(String uuid,ProductDto productDTO){
-            Optional<Product> optionalItem = Optional.ofNullable(productRepository.findByUuid(uuid));
+        public void update(String uuid,ProductDto productDTO) {
 
-            if(optionalItem.isPresent()){
-                Product updateProduct = optionalItem.get();
-                productRepository.updateItemByUUID(uuid, productDTO.getName(),
-                        productDTO.getPrice(), productDTO.getStockQuantity(),
-                        productDTO.getBoardwriter(),
-                        productDTO.getProductname());
+            TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+            try {
+                Optional<Product> optionalItem = Optional.ofNullable(productRepository.findByUuid(uuid));
+
+                if (optionalItem.isPresent()) {
+                    Product updateProduct = optionalItem.get();
+                    productRepository.updateItemByUUID(uuid, productDTO.getName(),
+                            productDTO.getPrice(), productDTO.getStockQuantity(),
+                            productDTO.getBoardwriter(),
+                            productDTO.getProductname());
+                }
+
+                transactionManager.commit(status); // 성공 시 커밋
+            } catch (Exception e) {
+                transactionManager.rollback(status); // 실패 시 롤백
+                throw new IllegalStateException(e);
             }
         }
         @Transactional
         public void delete(String uuid){
+            TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+            try {
             Optional<Product> optionalItem = Optional.ofNullable(productRepository.findByUuid(uuid));
 
             if(optionalItem.isPresent()){
                 Product product = optionalItem.get();
                 productRepository.delete(product);
             }
+                transactionManager.commit(status); // 성공 시 커밋
+            } catch (Exception e) {
+                transactionManager.rollback(status); // 실패 시 롤백
+                throw new IllegalStateException(e);
+            }
         }
 }
+
